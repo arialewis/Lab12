9dfa3c1f089898fd71ef6772c5a66dda6291d1d8
Aria Lewis <arialewis@ou.edu>
2018-11-15 15:42:10 -0600

    Drawable interface completed

---
+++ Drawable.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+
+/**
+ * Drawable interface 
+ * 
+ * @author Aria Lewis
+ * @version 2018-11-12
+ * Lab 11
+ */
+
+public interface Drawable 
+{
+	/**
+	 * Abstract method for drawing a Graphic
+	 * 
+	 * @param g Graphics object
+	 */
+	public abstract void draw(Graphics g);
+}
